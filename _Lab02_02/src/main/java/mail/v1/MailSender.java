df3caa16f8a5cdbcae8788c8918708dcb4e55e0b
lshadown <gruzewski94@gmail.com>
2017-12-06 18:28:59 +0100

    - add MailSender v1

---
+++ _Lab02_02/src/main/java/mail/v1/MailSender.java
@@ -0,0 +1,52 @@
+package mail.v1;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class MailSender {
+    private String smtpHost = "localhost";
+
+    public void sendMessage(String from, String[] recipients, String subject, String message)
+            throws MessagingException {
+        boolean debug = false;
+
+        //Set the host smtp address
+        Properties props = new Properties();
+        props.put("mail.smtp.host", smtpHost);
+
+        // create some properties and get the default Session
+        Session session = Session.getDefaultInstance(props, null);
+        session.setDebug(debug);
+
+        // create a message
+        Message msg = new MimeMessage(session);
+
+        // set the from and to address
+        InternetAddress addressFrom = new InternetAddress(from);
+        msg.setFrom(addressFrom);
+
+        InternetAddress[] addressTo = new InternetAddress[recipients.length];
+        for (int i = 0; i < recipients.length; i++) {
+            addressTo[i] = new InternetAddress(recipients[i]);
+        }
+        msg.setRecipients(Message.RecipientType.TO, addressTo);
+
+
+        // Setting the Subject and Content Type
+        msg.setSubject(subject);
+        msg.setContent(message, "text/html");
+        Transport.send(msg);
+    }
+
+    public String getSmtpHost() {
+        return smtpHost;
+    }
+
+    public void setSmtpHost(String smtpHost) {
+        this.smtpHost = smtpHost;
+    }
+}
